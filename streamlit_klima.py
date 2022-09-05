25060622052f3bd60877b93972b95e5f2daf8736
jhaalbu <31095620+jhaalbu@users.noreply.github.com>
2022-09-05 14:44:08 +0200

    Delete streamlit_klima.py

--- streamlit_klima.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.markdown('<a href="https://jhaalbu-av-kllima-2-streamlit-app-9x0fnj.streamlitapp.com/" target="_self">Klimascript er flytta hit</a>', unsafe_allow_html=True)
