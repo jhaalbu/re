db810301339c099eb195d071148cc405daeb1e86
jhaalbu <31095620+jhaalbu@users.noreply.github.com>
2022-09-05 14:28:07 +0200

    redrirect

---
+++ streamlit_klima.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown('<a href="https://jhaalbu-av-kllima-2-streamlit-app-9x0fnj.streamlitapp.com/" target="_self">Klimascript er flytta hit</a>', unsafe_allow_html=True)
