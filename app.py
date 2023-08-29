ea35a2b919e1fea582188d901966a080e2ab8d59
Jan Helge Aalbu <31095620+jhaalbu@users.noreply.github.com>
2023-08-29 12:49:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.markdown('<a href="https://av-klima.streamlitapp.com/" target="_self">Klimascript er flytta hit</a>', unsafe_allow_html=True)
+st.markdown('<a href="https://nve-av-klima.azurewebsites.net/" target="_self">Klimascript er flytta hit</a>', unsafe_allow_html=True)
